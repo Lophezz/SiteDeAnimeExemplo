28023b013f3cdc81459f2c090da955a506d6c957
Lophezz <lopes.matheus334@gmail.com>
2022-08-30 20:02:14 -0300

    Site de Anime finalizado

---
+++ public/service-worker-minimum-to-intall-pwa.js
@@ -0,0 +1,31 @@
+self.addEventListener('install', event => {
+    console.log('sw ./ => installing...');
+
+    // cache a cat SVG
+    event.waitUntil(
+        caches.open('static-v1').then(cache => cache.add('Index.html'))
+    );
+    
+    console.log("sw ./ =>  install event detected e Index.html cacheado!!!");
+    
+});
+
+
+self.addEventListener('activate', event => {
+    console.log('sw ./ => Evento activate ocorreu, agora pronto pra interceptar fetches');
+});
+
+
+self.addEventListener('fetch', event => {
+    console.log("sw ./ => Detectei um evento fetch para o recurso abaixo:");
+    console.log("sw ./ => "+event.request.url);
+    
+    const url = new URL(event.request.url);
+    
+
+    if (url.origin == location.origin && url.pathname == 'Index.html') {
+        event. respondWith(caches.match('Index.html'));
+    }
+});
+
+
